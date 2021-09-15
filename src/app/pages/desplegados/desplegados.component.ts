4f92d5c3d4d69d387561139b5f255e7ababddcf3
jcramosg <juancarlosramosg@gmail.com>
2021-09-14 22:59:41 -0500

    Template listo sin rutas

---
+++ src/app/pages/desplegados/desplegados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desplegados',
+  templateUrl: './desplegados.component.html',
+  styleUrls: ['./desplegados.component.css']
+})
+export class DesplegadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
